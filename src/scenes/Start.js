1342e96ee24a8aae2e4eed610fa1971bf160e342
whodell24 <whodell@ucsc.edu>
2025-11-06 14:43:39 -0800

    basic movement + animation

--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -15,11 +15,48 @@ export class Start extends Phaser.Scene {
         
         this.player = this.physics.add.sprite(50,100,'player',1);
         this.player.setScale(4);
+
+        this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE, true, true);
+        this.shift = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT, true, true);
+        this.left = this.input.keyboard.addKey("A", true, true);
+        this.right = this.input.keyboard.addKey("D", true, true);
+
+        this.anims.create({
+            key: 'idle',
+            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 2 }),
+            frameRate: 8,
+            repeat: -1
+        });
+        this.anims.create({
+            key: 'moving',
+            frames: this.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
+            frameRate: 8,
+            repeat: -1
+        });
     }
 
     update(time) {
         let dt = (time - this.last_time)/1000;
         this.last_time = time;
+
+        this.player.body.setVelocityX(0);
+
+        if (this.left.isDown && !(this.right.isDown))
+        {
+            this.player.body.setVelocityX(-300);
+            this.player.setFlipX(true);
+        }
+        else if (this.right.isDown && !(this.left.isDown))
+        {
+            this.player.body.setVelocityX(300);
+            this.player.setFlipX(false);
+        }
+
+        if(!(this.left.isDown || this.right.isDown)) {
+            this.player.anims.play('idle',true);
+        } else {
+            this.player.anims.play('moving',true);
+        }
         
     }
     
