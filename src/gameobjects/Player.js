ff423b4666cdd14a41d863cf0bea55580dd37c4f
Max <max.marika.brock@gmail.com>
2025-11-06 18:05:35 -0800

    moved player logic to it's own class

---
+++ src/gameobjects/Player.js
@@ -0,0 +1,86 @@
+export class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y, texture, frame) {
+        super(scene, x, y, texture, frame);
+
+        // Add player to scene and enable physics
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        // Controls
+        this.space = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.left = scene.input.keyboard.addKey("A");
+        this.right = scene.input.keyboard.addKey("D");
+
+        // Movement 
+        this.coyote = 0;
+        this.grounded = false;
+        this.djump = true;
+
+        // Animation setup
+        this.createAnimations(scene);
+    }
+
+    createAnimations(scene) {
+        scene.anims.create({
+            key: 'idle',
+            frames: scene.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
+            frameRate: 5,
+            repeat: -1
+        });
+
+        scene.anims.create({
+            key: 'moving',
+            frames: scene.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
+            frameRate: 10,
+            repeat: -1
+        });
+    }
+
+    update(dt) {
+        // Horizontal movement
+        this.body.setVelocityX(0);
+        if (this.left.isDown && !this.right.isDown) {
+            this.body.setVelocityX(-200);
+            this.setFlipX(true);
+        } else if (this.right.isDown && !this.left.isDown) {
+            this.body.setVelocityX(200);
+            this.setFlipX(false);
+        }
+
+        // Jumping
+        if (Phaser.Input.Keyboard.JustDown(this.space)) {
+            if (this.grounded) {
+                this.body.setVelocityY(-150);
+            } else if (this.djump) {
+                this.body.setVelocityY(-250);
+                this.djump = false;
+            }
+        }
+
+        // Coyote time and grounded logic
+        if (!this.body.blocked.down) {
+            this.coyote -= dt;
+            if (this.coyote < 0) this.grounded = false;
+        } else {
+            this.coyote = 0.1;
+            this.grounded = true;
+            this.djump = true;
+        }
+
+        // Gravity adjustments
+        if (this.grounded) {
+            this.body.setGravityY(0);
+        } else if (this.body.velocity.y > 0) {
+            this.body.setGravityY(800);
+        } else {
+            this.body.setGravityY(600);
+        }
+
+        // Animation
+        if (!(this.left.isDown || this.right.isDown)) {
+            this.anims.play('idle', true);
+        } else {
+            this.anims.play('moving', true);
+        }
+    }
+}
--- src/scenes/Start.js
+++ src/scenes/Start.js
@@ -1,3 +1,5 @@
+import { Player } from '../gameobjects/Player.js';
+
 export class Start extends Phaser.Scene {
 
     constructor() {
@@ -23,91 +25,95 @@ export class Start extends Phaser.Scene {
         this.obslayer = this.map.createLayer("Obstacals", this.tileset, 0, 0);
         //this.map.setCollision([ 20, 48 ]);
         
-        this.player = this.physics.add.sprite(50,200,'player',1);
-        //this.player.setScale(4);
-        this.player.setDepth(2);
+        // this.player = this.physics.add.sprite(50,200,'player',1);
+        // //this.player.setScale(4);
+        // this.player.setDepth(2);
 
         this.platlayer.setCollisionBetween(1,1767);
+        // this.physics.add.collider(this.platlayer, this.player);
+
+        // this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE, true, true);
+        // this.shift = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT, true, true);
+        // this.left = this.input.keyboard.addKey("A", true, true);
+        // this.right = this.input.keyboard.addKey("D", true, true);
+
+        // this.coyote = 0;
+        // this.grounded = false;
+        // this.djump = true;
+
+        // this.anims.create({
+        //     key: 'idle',
+        //     frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
+        //     frameRate: 5,
+        //     repeat: -1
+        // });
+        // this.anims.create({
+        //     key: 'moving',
+        //     frames: this.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
+        //     frameRate: 10,
+        //     repeat: -1
+        // });
+        this.player = new Player(this, 50, 200, 'player', 1);
+        this.player.setDepth(2);
         this.physics.add.collider(this.platlayer, this.player);
-
-        this.space = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE, true, true);
-        this.shift = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SHIFT, true, true);
-        this.left = this.input.keyboard.addKey("A", true, true);
-        this.right = this.input.keyboard.addKey("D", true, true);
-
-        this.coyote = 0;
-        this.grounded = false;
-        this.djump = true;
-
-        this.anims.create({
-            key: 'idle',
-            frames: this.anims.generateFrameNumbers('player', { start: 0, end: 1 }),
-            frameRate: 5,
-            repeat: -1
-        });
-        this.anims.create({
-            key: 'moving',
-            frames: this.anims.generateFrameNumbers('player', { start: 4, end: 7 }),
-            frameRate: 10,
-            repeat: -1
-        });
     }
 
     update(time) {
         let dt = (time - this.last_time)/1000;
         this.last_time = time;
+        this.player.update(dt);
 
-        //Movement Handling
-        this.player.body.setVelocityX(0);
+        // //Movement Handling
+        // this.player.body.setVelocityX(0);
 
-        if (this.left.isDown && !(this.right.isDown))
-        {
-            this.player.body.setVelocityX(-200);
-            this.player.setFlipX(true);
-        }
-        else if (this.right.isDown && !(this.left.isDown))
-        {
-            this.player.body.setVelocityX(200);
-            this.player.setFlipX(false);
-        }
+        // if (this.left.isDown && !(this.right.isDown))
+        // {
+        //     this.player.body.setVelocityX(-200);
+        //     this.player.setFlipX(true);
+        // }
+        // else if (this.right.isDown && !(this.left.isDown))
+        // {
+        //     this.player.body.setVelocityX(200);
+        //     this.player.setFlipX(false);
+        // }
 
-        if(Phaser.Input.Keyboard.JustDown(this.space)) {
-            if(this.grounded) {
-                this.player.body.setVelocityY(-150);
-            } else if (this.djump) {
-                this.player.body.setVelocityY(-250);
-                this.djump = false;
-            }
-        }
+        // if(Phaser.Input.Keyboard.JustDown(this.space)) {
+        //     if(this.grounded) {
+        //         this.player.body.setVelocityY(-150);
+        //     } else if (this.djump) {
+        //         this.player.body.setVelocityY(-250);
+        //         this.djump = false;
+        //     }
+        // }
 
-        //Grounded and Coyote Time
-        if(!this.player.body.blocked.down)
-        {
-            this.coyote -= dt;
-            if(this.coyote < 0) {
-                this.grounded = false;
-            }
-        } else {
-            this.coyote = 0.1;
-            this.grounded = true;
-            this.djump = true;
-        }
+        // //Grounded and Coyote Time
+        // if(!this.player.body.blocked.down)
+        // {
+        //     this.coyote -= dt;
+        //     if(this.coyote < 0) {
+        //         this.grounded = false;
+        //     }
+        // } else {
+        //     this.coyote = 0.1;
+        //     this.grounded = true;
+        //     this.djump = true;
+        // }
 
-        //Gravity
-        if(this.grounded) { 
-            this.player.body.setGravityY(0);
-        } else if(this.player.body.velocity.y > 0) {
-            this.player.body.setGravityY(800);
-        } else {
-            this.player.body.setGravityY(600);
-        }
+        // //Gravity
+        // if(this.grounded) { 
+        //     this.player.body.setGravityY(0);
+        // } else if(this.player.body.velocity.y > 0) {
+        //     this.player.body.setGravityY(800);
+        // } else {
+        //     this.player.body.setGravityY(600);
+        // }
 
-        //Animation Handling
-        if(!(this.left.isDown || this.right.isDown)) {
-            this.player.anims.play('idle',true);
-        } else {
-            this.player.anims.play('moving',true);
-        }
+        // //Animation Handling
+        // if(!(this.left.isDown || this.right.isDown)) {
+        //     this.player.anims.play('idle',true);
+        // } else {
+        //     this.player.anims.play('moving',true);
+        // }
         
     }
 }
